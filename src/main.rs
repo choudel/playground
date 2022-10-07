9d9cdb21872e54f2293083ad9854554c87541807
choudel <chouaibh@gmail.com>
2022-10-07 10:06:50 +0200

    vectors and iteration

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,17 @@
-use model::Hands;
 
-mod model {
-    pub trait Displayable{
-        fn display(&self)->String;
-    }
-    enum Fruit {
-        Apple,
-        Banana,
-        Kiwi,
-    }
-    impl Displayable for Fruit {
-        fn display(&self) -> String {
-            match self {
-                Fruit::Apple => "an apple".to_owned(),
-                Fruit::Banana => "a banana".to_owned(),
-                Fruit::Kiwi => "a kiwi".to_owned(),
-            }
-        }
-    }
-    pub struct Hands {
-        left: Option<Fruit>,
-        right: Option<Fruit>,
-    }
-    impl Hands {
-        pub fn juggle(mut self) -> Self {
-            println!("let's juggle");
-            let air = self.left;
-            self.left = self.right;
-            self.right = air;
-            self
-        }
-        pub fn report(&self) {
-            report_item(&self.left, "left");
-            report_item(&self.right, "right");
-        }
-        pub fn new() -> Self {
-            Self {
-                left: Some(Fruit::Apple),
-                right: Some(Fruit::Banana),
-            }
-        }
-    }
-   
-    
-        pub fn report_item<T:Displayable>(item:&Option<T>, which: &str) {
-            match item {
-                Some(what) => {
-                    println!("{} hand is holding {}", which, what.display())
-                }
-                _ => {
-                    println!("{} hand is holding nothing", which)
-                }
-            }
-    
-    }
-}
 
 fn main() {
-    let mut hands = Hands::new();
-
-    hands.report();
-
-    hands = hands.juggle();
-
-    hands.report();
+    let primes = vec![2,3,5];
+    let mut i =0;
+    while i < primes.len(){
+        println!("Prime #{}: {}",i+1 , primes[i]);
+        i += 1;
+    }
+    let mut primes_iter = primes.iter().enumerate();
+    while let Some((i,prime))=primes_iter.next(){
+        println!("Prime is #{} : {}",i+1, prime);
+    }
+    for (i,prime)in primes.iter().enumerate(){
+        println!("Prime is #{} : {}",i+1, prime);
+    }
 }
--- src/printing/time_stuff.rs
+++ src/printing/time_stuff.rs
@@ -1,3 +1,57 @@
-pub fn give_us_the_time(){
-    println!("The current date/time is {}", chrono::Local::now());
+use model::Hands;
+
+mod model {
+    pub trait Displayable{
+        fn display(&self)->String;
+    }
+    enum Fruit {
+        Apple,
+        Banana,
+        Kiwi,
+    }
+    impl Displayable for Fruit {
+        fn display(&self) -> String {
+            match self {
+                Fruit::Apple => "an apple".to_owned(),
+                Fruit::Banana => "a banana".to_owned(),
+                Fruit::Kiwi => "a kiwi".to_owned(),
+            }
+        }
+    }
+    pub struct Hands {
+        left: Option<Fruit>,
+        right: Option<Fruit>,
+    }
+    impl Hands {
+        pub fn juggle(mut self) -> Self {
+            println!("let's juggle");
+            let air = self.left;
+            self.left = self.right;
+            self.right = air;
+            self
+        }
+        pub fn report(&self) {
+            report_item(&self.left, "left");
+            report_item(&self.right, "right");
+        }
+        pub fn new() -> Self {
+            Self {
+                left: Some(Fruit::Apple),
+                right: Some(Fruit::Banana),
+            }
+        }
+    }
+   
+    
+        pub fn report_item<T:Displayable>(item:&Option<T>, which: &str) {
+            match item {
+                Some(what) => {
+                    println!("{} hand is holding {}", which, what.display())
+                }
+                _ => {
+                    println!("{} hand is holding nothing", which)
+                }
+            }
+    
+    }
 }
