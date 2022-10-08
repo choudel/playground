3c540dd09e4f0502023b98ea04f1c9418f25f9d0
choudel <chouaibh@gmail.com>
2022-10-08 07:30:37 +0200

    hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-
+use std::collections::HashMap;
 
 fn main() {
-    let primes = vec![2,3,5];
-    let mut i =0;
-    while i < primes.len(){
-        println!("Prime #{}: {}",i+1 , primes[i]);
-        i += 1;
-    }
-    let mut primes_iter = primes.iter().enumerate();
-    while let Some((i,prime))=primes_iter.next(){
-        println!("Prime is #{} : {}",i+1, prime);
-    }
-    for (i,prime)in primes.iter().enumerate(){
-        println!("Prime is #{} : {}",i+1, prime);
+    let mut grid = HashMap::new();
+    grid.insert((2, 6), "three");
+    grid.insert((4, 7), "rock");
+    grid.entry((4,7)).or_insert("empty");
+    grid.remove(&(4,7));
+
+        let coords = (2,6);
+        if let Some(cell)=grid.get(&coords){
+            println!("{}: {:?}", cell, coords)
+        }else{
+            println!("nothig in {:?}",coords)
+        }
+    for (key, value) in &grid {
+        println!("{}: {:?}", value, key)
     }
 }
