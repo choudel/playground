e9dc48f8013028a328883ab073f640cd68548247
choudel <chouaibh@gmail.com>
2022-10-08 11:42:08 +0200

    lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
-use std::collections::HashMap;
+struct OurIterator<'a> {
+    nums: &'a [i32],
+    i: usize,
+    x: i32,
+}
 
-fn main() {
-    let mut grid = HashMap::new();
-    grid.insert((2, 6), "three");
-    grid.insert((4, 7), "rock");
-    grid.entry((4,7)).or_insert("empty");
-    grid.remove(&(4,7));
-
-        let coords = (2,6);
-        if let Some(cell)=grid.get(&coords){
-            println!("{}: {:?}", cell, coords)
-        }else{
-            println!("nothig in {:?}",coords)
-        }
-    for (key, value) in &grid {
-        println!("{}: {:?}", value, key)
+impl<'a> OurIterator<'a> {
+    fn new(nums: &'a [i32], x: i32) -> Self {
+        Self { nums, i: 0, x }
     }
+}
+
+impl<'a> Iterator for OurIterator<'a> {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i < self.nums.len() {
+            let value = self.nums[self.i];
+            self.i += 1;
+            Some(value * self.x)
+        } else {
+            None
+        }
+    }
+}
+fn main() {
+    let mut nums = vec![1, 2, 3];
+
+    let mut nums2 = OurIterator::new(&nums, 2);
+    let mut nums3 = OurIterator::new(&nums, 3);
+    while let Some(n) = nums2.next() {
+        println!("{}", n);
+    }
+
+    while let Some(n) = nums3.next() {
+        println!("{}", n);
+    }
+    nums.push(5);
 }
