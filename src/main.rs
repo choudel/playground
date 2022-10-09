9f924e7fe2d367ae4d14e6f5e65a07cedcd71d08
choudel <chouaibh@gmail.com>
2022-10-09 07:56:06 +0200

    closures

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,22 @@
-struct OurIterator<'a> {
-    nums: &'a [i32],
-    i: usize,
-    x: i32,
+fn double(n: i32) -> i32 {
+    n * 2
+}
+fn triple(n: i32) -> i32 {
+    n * 3
 }
 
-impl<'a> OurIterator<'a> {
-    fn new(nums: &'a [i32], x: i32) -> Self {
-        Self { nums, i: 0, x }
-    }
-}
-
-impl<'a> Iterator for OurIterator<'a> {
-    type Item = i32;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.i < self.nums.len() {
-            let value = self.nums[self.i];
-            self.i += 1;
-            Some(value * self.x)
-        } else {
-            None
-        }
-    }
+fn make_multiplier(mut x: i32) -> impl FnMut(i32) -> i32 {
+    
+        move |n|{
+            x +=1;
+            n * x
+        }     
 }
 fn main() {
-    let mut nums = vec![1, 2, 3];
-
-    let mut nums2 = OurIterator::new(&nums, 2);
-    let mut nums3 = OurIterator::new(&nums, 3);
-    while let Some(n) = nums2.next() {
-        println!("{}", n);
+    let nums = vec![1, 2, 3];
+    let nums_as_iter = nums.into_iter();
+    let multiplied = nums_as_iter.map(make_multiplier(2));
+    for l in multiplied {
+        println!("{}", l)
     }
-
-    while let Some(n) = nums3.next() {
-        println!("{}", n);
-    }
-    nums.push(5);
 }
