8038385ca9d5b7445cabdf072caeb60341a3d1eb
choudel <chouaibh@gmail.com>
2022-11-16 22:30:32 +0100

    async

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,27 @@
-use std::{rc::Rc, vec};
-
-struct Node {
-    value: &'static str,
-    edges:Vec<Rc<Node>>
-}
-impl Node {
-    fn display(&self) {
-        println!("Value:\n_ \n_*|{}", self.value);
-        for edge in &self.edges{
-          edge.display();
-        }
-        println!("--------UP--------")
+use std::time::Duration;
+use tokio::time::sleep;
+#[tokio::main]
+async fn main() {
+    let mut handles = vec![];
+    for i in 0..2 {
+        let handle = tokio::spawn(async move {
+            my_function(i).await;
+        });
+        handles.push(handle);
     }
+    for handle in handles {
+      handle.await.unwrap();
+    }
+    
 }
-fn main() {
-    let e = Rc::new(Node{
-      value:"e",
-      edges: vec![]
-    });
-    let d = Rc::new(Node{
-      value:"d",
-      edges: vec![e.clone()]
-    }) ;
-    let a =Node{
-      value:"a",
-      edges:vec![
-        Rc::new(Node{
-          value:"b",
-          edges:vec![d.clone()],
-        }),
-        Rc::new(Node{
-          value:"c",
-          edges:vec![d.clone(),e.clone()],
-        }),
-        d,
-        e
-      ]
-    };
-    a.display();
+async fn my_function(i:i32) {
+    println!("[{i}] I'm an async function!");
+    let s1 = read_from_database().await;
+    println!("[{i}] First result : {s1}");
+    let s2 = read_from_database().await;
+    println!("[{i}] Second result: {s2}");
+}
+async fn read_from_database() -> String {
+    sleep(Duration::from_millis(50)).await;
+    "DB Result".to_owned()
 }
