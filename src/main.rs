7b5ab3c63ef6e31b3e51f8933ddea93b11a58474
choudel <chouaibh@gmail.com>
2022-11-20 21:26:58 +0100

    Hashmaps use

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,49 @@
-enum Titles {
-    Maintenance,
-    Marketing,
-    Managers,
-    Supervisor,
-    Cook,
-    Technician,
-}
-enum Status {
-    Active,
-    Terminated,
-}
-struct Employee {
-    title: Titles,
-    status: Status,
-}
-fn access(val: Employee) -> Result<(), String> {
-    match val.status {
-        Status::Terminated => Err("Denied access".to_string()),
-        Status::Active => match val.title {
-            Titles::Maintenance | Titles::Marketing | Titles::Managers => Ok(()),
-            Titles::Supervisor | Titles::Cook | Titles::Technician => {
-                Err("Denied access".to_string())
-            }
-        },
-    }
-}
-fn print_result(val: Employee) -> Result<(), String> {
-    let attempt_access = access(val)?;
-    println!("Access granted {attempt_access:?}");
-    Ok(())
+use std::collections::HashMap;
+#[derive(Debug)]
+struct Item {
+    name: String,
+    in_stock: i32,
 }
 
 fn main() {
-    let mike = Employee {
-        title: Titles::Cook,
-        status: Status::Active,
-    };
-    match print_result(mike) {
-        Err(e)=>println!("access denied : {e:?}"),
-        _=>()
+    let mut store = HashMap::new();
+    store.insert(
+        1,
+        Item {
+            name: "Chairs".to_string(),
+            in_stock: 5,
+        },
+    );
+    store.insert(
+        2,
+        Item {
+            name: "Beds".to_string(),
+            in_stock: 3,
+        },
+    );
+    store.insert(
+        3,
+        Item {
+            name: "Tables".to_string(),
+            in_stock: 2,
+        },
+    );
+    store.insert(
+        4,
+        Item {
+            name: "Couches".to_string(),
+            in_stock: 0,
+        },
+    );
+    let mut total=0;
+    for (k, item) in store.iter() {
+        if item.in_stock == 0 {
+            println!("Out of stock")
+        } else {
+            println!("{:?}",item)
+        }
+        total = total+item.in_stock;
     }
+    println!("{total}");
+
 }
