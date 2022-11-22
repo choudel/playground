4285401b1c5faa6a9da5de8b83c6e44f292807f0
choudel <chouaibh@gmail.com>
2022-11-22 23:46:21 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
-#[derive(Debug)]
-struct User {
-    user_id: i32,
-    name: String,
-}
-fn find_user(name: &str) -> Option<i32> {
-    let name = name.to_lowercase();
-    match name.as_str() {
-        "sam" => Some(1),
-        "matt" => Some(5),
-        "katie" => Some(9),
-        _ => None,
+fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
+    if n < lower {
+        lower
+    } else if n > upper {
+        upper
+    } else {
+        n
     }
 }
-fn main() {
-    let username = "patt";
-    let user = find_user(username).map(|user_id| User {
-        user_id,
-        name: username.to_owned(),
-    });
-    println!("{user:?}");
-    match user {
-        Some(user) => println!("{user:?}"),
-        None => println!("user not found"),
+fn div(a:i32,b:i32)->Option<i32>{
+    if b==0{
+        None
+    }else{
+        Some(a/b)
+    }
+     
+}
+fn concat(first:&str , second:&str)->String{
+    format!("{} {}",first,second)
+}
+fn main() {}
+#[cfg(test)]
+mod test {
+
+    use crate::{clamp, div,concat};
+
+    #[test]
+    fn check_clamp_outside(){
+        let result =clamp(6, 3, 5);
+        let expected = 5;
+        assert_eq!(result,expected,"no that's not it")
+    }
+    #[test]
+    fn check_clamp_middle(){
+        let result =clamp(4, 3, 5);
+        let expected = 4;
+        assert_eq!(result,expected,"no that's not it")
+    }
+    #[test]
+    fn check_div(){
+        let result = div(6, 1);
+        let expected = Some(6);
+        assert_eq!(result,expected,"no that")
+    }
+    #[test]
+    fn check_div_zero(){
+        let result = div(6, 0);
+        let expected = None;
+        assert_eq!(result,expected,"no that")
+    }
+   
+    #[test]
+    fn check_test(){
+        let result= concat("a","b");
+        let expected=String::from("a b");
+        assert_eq!(result,expected,"should be placed a space")
     }
 }
