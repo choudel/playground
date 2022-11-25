37526702df04578440d50dc947e3ab09377e1604
choudel <chouaibh@gmail.com>
2022-11-25 21:05:23 +0100

    bill program

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,113 @@
 use std::io;
-enum PowerState {
-    Off,
-    Hibernate,
-    Sleep,
-    Reboot,
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name: String,
+    amount: f64,
 }
-impl PowerState {
-    fn new(input: &str) -> Option<PowerState> {
-        let recieved = input.trim().to_lowercase();
-        match recieved.as_str() {
-            "off" => Some(PowerState::Off),
-            "hibernate" => Some(PowerState::Hibernate),
-            "sleep" => Some(PowerState::Sleep),
-            "reboot" => Some(PowerState::Reboot),
-            _ => None,
+pub struct Bills {
+    inner: Vec<Bill>,
+}
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+    fn add(&mut self,bill:Bill){
+        self.inner.push(bill);
+    }
+    fn get_all(&self)->Vec<&Bill>{
+        self.inner.iter().collect()
+    }
+}
+mod menu{
+use crate::{Bill,Bills,get_input, get_bill_amount};
+    pub fn add_bill(bills:&mut Bills){
+        println!("Bill name:");
+        let name = match get_input() {
+            Some(input)=>input,
+            None=>return
+        };
+        let amount = match get_bill_amount() {
+            Some(amount)=>amount,
+            None=>return
+        };
+        let bill = Bill{name:name,amount:amount};
+        bills.add(bill);
+        println!("Bill added")
+    }
+    pub fn view_bills(bills:&Bills){
+        for bill in bills.get_all(){
+            println!("{bill:?}");
         }
     }
 }
-fn print_action(state:PowerState){
-  use PowerState::*;
-  match state{
-    Off=>println!("Offing"),
-    Hibernate=>println!("Zzzzz"),
-    Sleep=>println!("slpzzz"),
-    Reboot=>println!("bootex"),
-  }
+impl Bill {
+    pub fn view_bill(&self) {
+        println!(
+            "the bill is in the name of : {} with the amount of :{}",
+            self.name, self.amount
+        )
+    }
+}
+pub fn get_input() -> Option<String> {
+    let mut buff = String::new();
+    while io::stdin().read_line(&mut buff).is_err() {
+        println!("please enter your data again")
+    }
+    let input = buff.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+pub fn get_bill_amount()->Option<f64>{
+    println!("amount:");
+    loop {
+        let input = match get_input() {
+            Some(input)=>input,
+            None => return None,
+        };
+        if &input==""{
+            return None;
+        } 
+        let parsed_input:Result<f64,_> = input.parse();
+        match parsed_input{
+            Ok(amount)=> return Some(amount),
+            Err(_)=>println!("Please enter a number")
+        }
+    }
+}
+
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+impl MainMenu {
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBill),
+            _ => None,
+        }
+    }
+    fn show() {
+        println!("");
+        println!(" == Bill Manager == ");
+        println!("1.Add Bill");
+        println!("2.View Bills");
+        println!("");
+        println!("Enter selection:  ")
+    }
 }
 fn main() {
-  let mut buff = String::new();
-  let state =io::stdin().read_line(&mut buff);
-  if state.is_ok(){
-    match PowerState::new(&buff){
-      Some(state)=> print_action(state),
-      None=>println!("invalid state" )
+    let mut bills=Bills::new();
+    loop {
+        MainMenu::show();
+        let input = get_input().expect("no data entered");
+        match MainMenu::from_str(input.as_str()){
+            Some(MainMenu::AddBill)=>menu::add_bill(&mut bills),
+            Some(MainMenu::ViewBill)=>menu::view_bills(&bills),
+            None=>return,
+        }
     }
-  }else {
-    println!("something went wrong")
-  }
-
 }
