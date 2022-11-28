1441bd257bdd752b0ab351751d100fff688fdb87
choudel <chouaibh@gmail.com>
2022-11-28 23:09:43 +0100

    trait objects

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
-#[derive(Debug)]
-enum ServicePriority {
-    High,
-    Standard,
-}
-trait Priority {
-    fn get_priority(&self) -> ServicePriority;
+
+trait Clicky{
+    fn click(&self);
 }
 #[derive(Debug)]
-struct ImportantGuest;
-impl Priority for ImportantGuest {
-    fn get_priority(&self) -> ServicePriority {
-        ServicePriority::High
+
+struct Keyboard;
+impl Clicky for Keyboard {
+    fn click(&self) {
+        println!("click clack")
     }
 }
+fn borrow_clicky(obj:&dyn Clicky){
+    obj.click();
+}
+fn move_clicky(obj:Box<dyn Clicky>){
+    obj.click();
+}
 #[derive(Debug)]
-struct Guest;
-impl Priority for Guest {
-    fn get_priority(&self) -> ServicePriority {
-        ServicePriority::Standard
+
+struct Mouse;
+impl Clicky for Mouse{
+    fn click(&self) {
+        println!("click");
     }
 }
-fn print_priority<T: Priority+std::fmt::Debug>(prio: T) {
-    let result = prio.get_priority();
-    println!("name is {:?} the priority is {:?}", prio, result);
+fn make_clicks(clickeys:Vec<Box<dyn Clicky>>){
+    for click in clickeys{
+     click.click();
+    }
 }
 fn main() {
-    let mathiew = Guest;
-    print_priority(mathiew);
+let keeb = Box::new(Keyboard);
+let mouse =Box::new(Mouse);
+let clickers: Vec<Box<dyn Clicky>> = vec![keeb,mouse];
+make_clicks(clickers);
 }
