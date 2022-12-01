6c22a363384b302c6297780ae3d3c6f8eaeb7648
choudel <chouaibh@gmail.com>
2022-12-01 01:54:27 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,49 @@
-#[derive(Debug)]
-struct Cards {
-    inner: Vec<IdCard>,
+use std::error;
+
+use thiserror::Error;
+
+#[derive(Debug,Error)]
+enum ProgramError{
+    #[error("menu error")]
+    Menu(#[from]MenuError),
+    #[error("math error")]
+    Math(#[from]MathError),
+
 }
-#[derive(Debug, PartialEq, Eq,PartialOrd, Ord)]
-enum City {
-    Barland,
-    Bazopolis,
-    Fooville,
+#[derive(Debug,Error)]
+enum MenuError{
+    #[error("menu item not found")]
+    NotFound
 }
-#[derive(Debug)]
-struct IdCard {
-    name: String,
-    age: u8,
-    city: City,
+#[derive(Debug,Error)]
+enum MathError{
+    #[error("divide by zero error")]
+    DivideByZero,
 }
-impl IdCard {
-    pub fn new(name: &str, age: u8, city: City) -> Self {
-        Self {
-            name: name.to_string(),
-            age,
-            city,
-        }
+fn pick_menu(choice:&str)->Result<i32,MenuError>{
+    match choice
+{
+    "1"=>Ok(1),
+    "2"=>Ok(2),
+    _=>Err(MenuError::NotFound)
+
+}}
+fn divide(a:i32,b:i32)-> Result<i32,MathError>{
+    if b != 0{
+        Ok(a/b)
+    }else{
+        Err(MathError::DivideByZero)
     }
 }
-fn new_ids() -> Cards {
-    Cards { inner: vec![
-        IdCard::new("Amy",1,City::Fooville),
-        IdCard::new("Matt",5,City::Barland),
-        IdCard::new("Bailee",54,City::Bazopolis),
-        IdCard::new("Anthoine",30,City::Bazopolis),
-    ] }
-}
-#[derive(Debug)]
-struct YoungPeople<'a>{
-inner:Vec<&'a IdCard>
-}
-impl <'a> YoungPeople<'a> {
-    fn living_in_fooville(&self)->Self{
-        Self { inner: self
-        .inner.iter()
-        .filter(|id| id.city == City::Fooville)
-        .map(|id|*id)
-        .collect()
-        }
+fn run(step:i32)->Result<(),ProgramError>{
+    if step == 1 {
+        pick_menu("1")?;
+    }else if step == 2 {
+        divide(1, 0)?;
     }
+    Ok(())
 }
 fn main() {
-    let ids = new_ids();
-    let young = YoungPeople{
-        inner: ids.inner.iter().filter(|id| id.age <= 20).collect(),
-    };
-    for id in ids.inner.iter(){
-        println!("{id:?}**")
-    }
-    for id in young.inner.iter(){
-        println!("{id:?}--")
-    }
-    for id in young.living_in_fooville().inner.iter(){
-        println!("{id:?}11")
-    }
+    println!("{:?}",run(1));
+    println!("{:?}",run(2));
 }
