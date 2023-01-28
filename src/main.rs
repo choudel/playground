fe930d220b1b673184beae1346b6708e79a0c17e
choudel <chouaibh@gmail.com>
2023-01-28 17:53:22 +0100

    oop

--- src/main.rs
+++ src/main.rs
@@ -6,58 +6,46 @@ mod error;
 mod prelude;
 mod utils;
 
-#[derive(Debug)]
-struct Task {
-    name:String,
-    url:String,
-    headers:Vec<(String,String)>
-}
-#[derive(Default,Clone)]
-struct BuilderTask{
-    name:Option<String>,
-    url:Option<String>,
-    headers:Vec<(String,String)>
-}
-impl BuilderTask {
-    fn new()-> Self{
-        BuilderTask::default()
-    }
-    fn name(mut self, name:impl Into<String>)-> Self{
-        self.name.insert(name.into());
-        self
-    }
-    fn url(mut self,url:impl Into<String>)-> Self{
-        self.url.insert(url.into());
-        self
-    }
-    fn headers(mut self,a:impl Into<String>,b:impl Into<String>)->Self{
-        self.headers.push((a.into(),b.into()));
-        self
-    }
-    fn builder(self)->Result<Task>{
-        let Some(url)= self.url else {
-                return Err(Error::Generic("No url".to_string()));
-        };
-        Ok(Task{
-            name:self.name.unwrap_or_else(||"homer".to_string()),
-            url,
-            headers:self.headers,
-        })
+struct Sedan;
+struct SUV;
+struct Hovercraft;
+trait LandCapable {
+    fn drive(&self) {
+        println!("Default drive")
     }
 }
-fn main() -> Result<()> {
-    for entry in read_dir("./")?.filter_map(|e| e.ok()) {
-        let entry: String = W(&entry).try_into()?;
-        println!("{entry:?}")
-    };
-    let tasky = BuilderTask::new()
-    .name("howdy")
-    .url("holla.com")
-    .headers("pol","cop");
-    let req = tasky.clone().builder()?;
-    println!("{:?}",req);
-    let req = tasky.headers("Rojo","Poko").builder()?;
-    println!("{:?}",req);
-    Ok(())
-    
+trait WaterCapable {
+    fn sail(&self) {
+        println!("Default sail")
+    }
+}
+trait Amphibius: LandCapable + WaterCapable {}
+impl LandCapable for Sedan {}
+impl LandCapable for SUV {}
+impl Amphibius for Hovercraft {}
+impl WaterCapable for Hovercraft {
+    fn sail(&self) {
+        println!("Hovercraft Sailing")
+    }
+}
+impl LandCapable for Hovercraft {
+    fn drive(&self) {
+        println!("Hovercraft driving")
+    }
+}
+
+fn road_trip(vehicle: &impl LandCapable) {
+    vehicle.drive()
+}
+fn traverse_frozen_lake(vehicle: &impl Amphibius) {
+    vehicle.drive();
+    vehicle.sail()
+}
+fn main() {
+    let hc = Hovercraft;
+    traverse_frozen_lake(&hc);
+    let car = Sedan;
+    road_trip(&car);
+    let suv = SUV;
+    road_trip(&suv)
 }
